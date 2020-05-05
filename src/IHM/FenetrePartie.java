947cf762ce6f8d0ffd18c4aafc78cdaab4a65c5b
scartelo <58155534+scartelo@users.noreply.github.com>
2020-05-05 13:59:33 +0200

    Commit

--- src/IHM/FenetrePartie.java
+++ src/IHM/FenetrePartie.java
@@ -1,25 +1,22 @@
 package IHM;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.LayoutManager;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class MainMenu extends JFrame {
+public class FenetrePartie extends JFrame {
+
 	private int largeur = 1200;
 	private int hauteur = 1000;
-	private String titre = "Diabllik";
+	private String titre = "Diaballik";
 	JPanel conteneurPrincipal = new JPanel();
 	JPanel panelNomJeu = new JPanel();
 	JPanel panelBoutonsMenu = new JPanel();
-	
-	
-	public MainMenu() {
+
+	public FenetrePartie() {
 		// TODO Auto-generated constructor stub
 		this.setTitle(titre);
 		this.setSize(largeur, hauteur);
@@ -27,25 +24,30 @@ public class MainMenu extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		conteneurPrincipal.setLayout(new BorderLayout());
-		JLabel labelTitre = new JLabel(titre);
+		JLabel labelTitre = new JLabel("Partie");
 		panelNomJeu.add(labelTitre);
-		
+
 		conteneurPrincipal.add(panelNomJeu, BorderLayout.NORTH);
-		conteneurPrincipal.add(panelBoutonsMenu, BorderLayout.CENTER);
-		
+		Plateau plateau = new Plateau();
+		plateau.setBounds(10, 10, 452, 465);
+		conteneurPrincipal.add(plateau, BorderLayout.CENTER);
+
 		this.setContentPane(conteneurPrincipal);
 		this.setVisible(true);
 	}
-	
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		SwingUtilities.invokeLater(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				  MainMenu mainMenu = new MainMenu();
-				
+				FenetrePartie fPartie = new FenetrePartie();
+
 			}
 		});
+
 	}
+
 }
--- src/IHM/Plateau.java
+++ src/IHM/Plateau.java
@@ -1,7 +1,12 @@
 package IHM;
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
+import java.awt.geom.RoundRectangle2D;
 import java.io.File;
 import java.io.IOException;
 
@@ -16,25 +21,25 @@ public class Plateau extends JComponent {
 	private int nbLignes = nbColonnes = 7;
 	private int hauteur;
 	private int largeur = hauteur = 7;
-	private int largeurCase = 100;
-	private ImageIcon imgCaseSansPoint, imgCaseAvecPoint, imgPionJ1SurCase, imgPionJ2SurCase, imgPionJ1AvecBalle,
+	private final int CASE_LARGEUR = 100;
+	private Image imgCaseSansPoint, imgCaseAvecPoint, imgPionJ1SurCase, imgPionJ2SurCase, imgPionJ1AvecBalle,
 			imgPionJ2AvecBalle;
 
 	public Plateau() {
-		imgCaseSansPoint = new ImageIcon(getClass().getResource("vide.png"));
-		imgCaseAvecPoint = new ImageIcon(getClass().getResource("vide.png"));
-		imgPionJ1SurCase = new ImageIcon(getClass().getResource("vide.png"));
-		imgPionJ2SurCase = new ImageIcon(getClass().getResource("vide.png"));
-		imgPionJ1AvecBalle = new ImageIcon(getClass().getResource("vide.png"));
-		imgPionJ2AvecBalle = new ImageIcon(getClass().getResource("vide.png"));
-
+		/*imgCaseSansPoint = new ImageIcon(getClass().getResource("src/IHM/Images/vide.png"));
+		imgCaseAvecPoint = new ImageIcon(getClass().getResource("/IHM/Images/vide.png"));
+		imgPionJ1SurCase = new ImageIcon(getClass().getResource("/IHM/Images/vide.png"));
+		imgPionJ2SurCase = new ImageIcon(getClass().getResource("/IHM/Images/vide.pgg"));
+		imgPionJ1AvecBalle = new ImageIcon(getClass().getResource("/IHM/Images/vide.png"));
+		imgPionJ2AvecBalle = new ImageIcon(getClass().getResource("/IHM/Images/vide.png"));*/
+		imgCaseSansPoint = lisImage("vide.png");
 	}
 	
 
 	private Image lisImage(String nom) {
 		Image img = null;
 		try {
-			img = ImageIO.read(new File("IHM/Images/" + nom));
+			img = ImageIO.read(new File("src/IHM/Images/" + nom));
 		} catch (IOException io) {
 			System.out.println("Chargment de l'image " + nom + " Impossible");
 			System.exit(1);
@@ -49,16 +54,37 @@ public class Plateau extends JComponent {
 	public void paintComponent(Graphics g) {
 		Graphics2D g2D = (Graphics2D) g;
 		ImageIcon img = null;
-		for (int i = 0; i < this.largeur; i++) {
-			for (int j = 0; j < this.hauteur; j++) {
-				int x, y;
-				y = j * largeurCase;
-				x = i * largeurCase;
+		boolean aUnPoint = true;
+		int pair = 0;
+		
+		//les cases
+		for (int i = 0; i < this.hauteur; i++) {
+			for (int j = 0; j < this.largeur; j++) {
+				g2D.setPaint(Color.LIGHT_GRAY);
+				if(pair%2 == 0) {
+					//g2D.setPaint(Color.LIGHT_GRAY);
+					g2D.fillOval( ((j+1) * CASE_LARGEUR) + (CASE_LARGEUR/2)-10, ((i+1) * CASE_LARGEUR) + (CASE_LARGEUR/2)-10,
+							10,10);
 				
-				img.paintIcon(null, g2D, x, y);
+					g2D.drawOval((j+1) * CASE_LARGEUR,(i+1) * CASE_LARGEUR, CASE_LARGEUR-10, CASE_LARGEUR-10);
+				}else {
+					g2D.drawOval((j+1) * CASE_LARGEUR,(i+1) * CASE_LARGEUR, CASE_LARGEUR-10, CASE_LARGEUR-10);
+				}
+				pair++;
 				
+				//g2D.fill(new Rectangle2D.Double((j+1) * CASE_LARGEUR,(i+1) * CASE_LARGEUR, CASE_LARGEUR, CASE_LARGEUR));
 			}
 		}
+		
+		// le cadre
+		g2D.setPaint(Color.black);
+		g2D.setStroke(new BasicStroke(1)); //largeur 2 pixels
+
+		g2D.draw(new RoundRectangle2D.Double(CASE_LARGEUR-10, CASE_LARGEUR-10, 
+				(CASE_LARGEUR*this.largeur)+10, (CASE_LARGEUR*this.hauteur)+10, 100, 100 ));
+		
+		// Libère toutes les ressources d'écran natives utilisées par cette fenêtre, ses sous-composants et tous ses enfants possédés.
+		g2D.dispose();
 	}
 
 }
