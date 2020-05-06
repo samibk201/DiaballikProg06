44477b4fbd533454e4f5c5b3d8e951fdb4eeda56
scartelo <58155534+scartelo@users.noreply.github.com>
2020-05-06 14:16:21 +0200

    Add files via upload

--- src/Moteur/Diaballik.java
+++ src/Moteur/Diaballik.java
@@ -1,10 +1,14 @@
 package Moteur;
+import java.util.Scanner;
 
-public class Diaballik {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class Diabalik {
+	public static void main (String [] args) {
+		Scanner scan = new Scanner (System.in);
+		System.out.println ("Entrez nom1:");
+		String nom1 = scan.nextLine ();
+		System.out.println ("Entrez nom2:");
+		String nom2 = scan.nextLine ();
+		Partie partie = new Partie (new JoueurHumain (1,nom1),new JoueurHumain (2,nom2),0);
+		partie.joue ();
 	}
-
 }
