4936ded68e7896d34dfdd104e4d5b43aa4ea641c
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2020-05-07 11:12:01 +0200

    Add files via upload

---
+++ src/IHM/Button.java
@@ -0,0 +1,55 @@
+package Vue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import java.awt.color.Color;
+
+import java.awt.Dimension;
+
+public class Button extends JLabel {
+
+    Button(String imagePath, String text, int largeur, int hauteur) {
+
+        super();
+
+        this.setText(text);
+        this.setHorizontalTextPosition(JLabel.CENTER);
+
+        try {
+            this.setIcon(new ImageIcon(imagePath));
+        } catch (Exception e) {
+            System.err.println("can't read image");
+        }
+
+        this.setPreferredSize(new Dimension(largeur, hauteur));
+    }
+
+    Button(String imagePath, String text) {
+
+        super();
+
+        this.setForeground(java.awt.Color.WHITE);
+        this.setText(text);
+        this.setHorizontalTextPosition(JLabel.CENTER);
+
+        try {
+                this.setIcon(new ImageIcon(imagePath));
+            } catch (Exception e) {
+                System.err.println("can't read image");
+            }
+
+    }
+
+    Button(String imagePath) {
+
+        super();
+
+        try {
+                this.setIcon(new ImageIcon(imagePath));
+            } catch (Exception e) {
+                System.err.println("can't read image");
+            }
+
+    }
+}
