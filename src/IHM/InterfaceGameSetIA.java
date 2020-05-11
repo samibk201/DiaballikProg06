a5f5783fb52f1d88361f742f9779b5bf07ef13e2
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-11 10:39:51 +0200

    Update InterfaceGameSetIA.java

--- src/IHM/InterfaceGameSetIA.java
+++ src/IHM/InterfaceGameSetIA.java
@@ -16,31 +16,33 @@ import javax.swing.JTextField;
 
 public class InterfaceGameSetIA extends JPanel{
 
+    JLabel title;
+
     JPanel panelPlat;
     JPanel panelName;
     JPanel panelAvatar;
     JPanel panelColor;
 
-    public ButtonGroup bgPlat;
-	public JRadioButton standard;
+    ButtonGroup bgPlat;
+    public JRadioButton standard;
     public JRadioButton melange;
     
-    public ButtonGroup bgAvatar = new ButtonGroup();
-    public JRadioButton a1 = new JRadioButton("Avatar 1");
-    public JRadioButton a2 = new JRadioButton("Avatar 2");
-    public JRadioButton a3 = new JRadioButton("Avatar 3");
+    ButtonGroup bgAvatar;
+    public JRadioButton a1;
+    public JRadioButton a2;
+    public JRadioButton a3;
 
-    public ButtonGroup bgColor = new ButtonGroup();
-    public JRadioButton white = new JRadioButton("Blanc");
-    public JRadioButton black = new JRadioButton("Noir");
+    ButtonGroup bgColor;
+    public JRadioButton white;
+    public JRadioButton black;
 
-    public JLabel plateau;
-    public JLabel name;
-    public JLabel avatar;
-    public JLabel color;
+    Button plateau;
+    Button name;
+    Button avatar;
+    Button color;
 
     public Button next;
-
+	
     InterfaceGameSetIA(){
         super();
     }
@@ -51,8 +53,19 @@ public class InterfaceGameSetIA extends JPanel{
         this.setBackground(new Color(53, 79, 82));
         this.setBounds((screen.width-600)/2, (screen.height-600)/2, 600, 600);
 
+        title = new JLabel("Paramètres de la partie");
+        title.setFont(new Font("Tahoma", Font.BOLD, 36));
+        JPanel flowTitle = new JPanel();
+        flowTitle.setLayout(new FlowLayout(FlowLayout.CENTER));
+        flowTitle.setBackground(new Color(53, 79, 82));
+        flowTitle.add(title);
+
         JTextField nameField = new JTextField("Joueur1");
         nameField.setColumns(10);
+
+        JPanel boxy = new JPanel();
+        boxy.setLayout(new BoxLayout(boxy, BoxLayout.Y_AXIS));
+        boxy.setBackground(new Color(53, 79, 82));
 
         this.next = new Button("ressources/ButtonImage/nextSR.png");
         
@@ -62,11 +75,16 @@ public class InterfaceGameSetIA extends JPanel{
         this.panelColor = new JPanel();
 
         panelPlat.setLayout(new BoxLayout(panelPlat, BoxLayout.LINE_AXIS));
-        this.plateau = new JLabel("Choix du plateau : ");
+        panelPlat.setBackground(new Color(53, 79, 82));
+        this.plateau = new Button("ressources/ButtonImage/button1.png","Configuration du plateau : ");
         this.bgPlat = new ButtonGroup();
         this.standard = new JRadioButton("Plateau standard");
         this.melange = new JRadioButton("Plateau mélangé");
+        standard.setForeground(Color.white);
+        melange.setForeground(Color.white);
         standard.setSelected(true);
+        standard.setBackground(new Color(53, 79, 82));
+        melange.setBackground(new Color(53, 79, 82));
         bgPlat.add(standard);
         bgPlat.add(melange);
         panelPlat.add(plateau);
@@ -75,17 +93,25 @@ public class InterfaceGameSetIA extends JPanel{
 
         this.panelName = new JPanel();
         panelName.setLayout(new BoxLayout(panelName, BoxLayout.LINE_AXIS));
-        this.name = new JLabel("Nom du joueur : ");
+        panelName.setBackground(new Color(53, 79, 82));
+        this.name = new Button("ressources/ButtonImage/button1.png","Nom du joueur : ");
         panelName.add(name);
         panelName.add(nameField);
 
         panelAvatar.setLayout(new BoxLayout(panelAvatar, BoxLayout.LINE_AXIS));
-        this.avatar = new JLabel("Choix de l'avatar : ");
+        panelAvatar.setBackground(new Color(53, 79, 82));
+        this.avatar = new Button("ressources/ButtonImage/button1.png","Avatar du joueur : ");
         this.bgAvatar = new ButtonGroup();
         this.a1 = new JRadioButton("Avatar 1");
         this.a2 = new JRadioButton("Avatar 2");
         this.a3 = new JRadioButton("Avatar 3");
+        a1.setForeground(Color.white);
+        a2.setForeground(Color.white);
+        a3.setForeground(Color.white);
         a1.setSelected(true);
+        a1.setBackground(new Color(53, 79, 82));
+        a2.setBackground(new Color(53, 79, 82));
+        a3.setBackground(new Color(53, 79, 82));
         bgAvatar.add(a1);
         bgAvatar.add(a2);
         bgAvatar.add(a3);
@@ -95,22 +121,30 @@ public class InterfaceGameSetIA extends JPanel{
         panelAvatar.add(a3);
 
         panelColor.setLayout(new BoxLayout(panelColor, BoxLayout.LINE_AXIS));
-        this.color = new JLabel("Couleur des pions : ");
+        panelColor.setBackground(new Color(53, 79, 82));
+        this.color = new Button("ressources/ButtonImage/button1.png","Couleur des pions : ");
         this.bgColor = new ButtonGroup();
         this.white = new JRadioButton("Blanc");
         this.black = new JRadioButton("Noir");
+        white.setForeground(Color.white);
+        black.setForeground(Color.white);
         white.setSelected(true);
+        white.setBackground(new Color(53, 79, 82));
+        black.setBackground(new Color(53, 79, 82));
         bgColor.add(white);
         bgColor.add(black);
         panelColor.add(color);
         panelColor.add(white);
-        panelColor.add(white);
-        
-        this.add(panelPlat);
-        this.add(panelName);
-        this.add(panelAvatar);
-        this.add(panelColor);
-        this.add(next);
+        panelColor.add(black);
+
+        boxy.add(flowTitle);
+        boxy.add(panelPlat);
+        boxy.add(panelName);
+        boxy.add(panelAvatar);
+        boxy.add(panelColor);
+        boxy.add(next);
+
+        this.add(boxy);
     }
     
 }
