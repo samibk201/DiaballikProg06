6c0169ff1fcbb7408450ec2ca425cec137d298d2
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-11 14:25:28 +0200

    Update InterfaceAccueil.java

--- src/IHM/InterfaceAccueil.java
+++ src/IHM/InterfaceAccueil.java
@@ -6,6 +6,8 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class InterfaceAccueil extends JPanel{
@@ -16,6 +18,8 @@ public class InterfaceAccueil extends JPanel{
     public Button tutoriel;
     public Button quit;
 
+    JLabel title;
+
     InterfaceAccueil() {
         super();
     }
@@ -25,16 +29,26 @@ public class InterfaceAccueil extends JPanel{
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         // acceuil.setLayout(new BoxLayout(acceuil, BoxLayout.PAGE_AXIS));
         this.setBackground(new Color(53, 79, 82));
-        this.setBounds((screen.width-600)/2, (screen.height-600)/2, 600, 600);
+        this.setBounds(0, 0, screen.width, screen.height);
 
-        //this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        title = new JLabel();
+        title.setIcon(new ImageIcon("ressources/ButtonImage/diaballik.png"));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         this.unJoueur = new Button("ressources/ButtonImage/jVSjSR(1).png");
+        unJoueur.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.deuxJoueur = new Button("ressources/ButtonImage/jVSiaSR.png");
+        deuxJoueur.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.enCours = new Button("ressources/ButtonImage/encoursSR.png");
+        enCours.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.tutoriel = new Button("ressources/ButtonImage/tutorielSR.png");
+        tutoriel.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.quit = new Button("ressources/ButtonImage/btnquitSR.png");
+        quit.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        this.add(title);
         this.add(unJoueur);
         this.add(deuxJoueur);
         this.add(enCours);
