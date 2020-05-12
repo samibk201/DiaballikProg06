3003bed14c27e2ff06329f73666c09ac2f08772b
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-12 13:50:44 +0200

    Update InterfaceTuto5.java

--- src/IHM/InterfaceTuto5.java
+++ src/IHM/InterfaceTuto5.java
@@ -29,6 +29,8 @@ public class InterfaceTuto5 extends JPanel{
 
     public Button next;
     public Button previous;
+    public Button home;
+    public Button volum;
 
     InterfaceTuto5(){
         super();
@@ -39,6 +41,14 @@ public class InterfaceTuto5 extends JPanel{
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         this.setBackground(new Color(53, 79, 82));
         this.setBounds(0, 0, screen.width, screen.height);
+
+        this.setLayout(new BorderLayout());
+
+        JPanel flow = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        home = new Button("ressources/ButtonImage/homeBSR.png");
+        volum = new Button("ressources/ButtonImage/volume.png");
+        flow.add(home);
+        flow.add(volum);
 
         panelTitle = new JPanel();
         panelTitle.setLayout(new FlowLayout(FlowLayout.CENTER));
@@ -97,7 +107,9 @@ public class InterfaceTuto5 extends JPanel{
         box.add(panelText);
         box.add(flowBut);
 
-        this.add(box);
+        this.add(flow, BorderLayout.NORTH);
+        this.add(box, BorderLayout.CENTER);
+        this.add(flowBut, BorderLayout.SOUTH);
     }
     
 }
