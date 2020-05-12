be8f29f827f115234e046d64ea8439acf5e7b608
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-12 19:30:26 +0200

    Update InterfaceTuto3.java

--- src/IHM/InterfaceTuto3.java
+++ src/IHM/InterfaceTuto3.java
@@ -11,6 +11,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.Toolkit;
 
