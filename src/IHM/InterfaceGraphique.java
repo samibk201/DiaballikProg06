65579e9346ae0b9a7fa75039de13dcc35a9f5931
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-12 13:08:46 +0200

    Update InterfaceGraphique.java

--- src/IHM/InterfaceGraphique.java
+++ src/IHM/InterfaceGraphique.java
@@ -8,8 +8,8 @@ import javax.swing.*;
 
 public class InterfaceGraphique implements Runnable {
 
-    public Button back;
-    public Button volum;
+    /*public Button back;
+    public Button volum;*/
 
 	InterfaceGraphique() {
 		// TODO
@@ -25,20 +25,33 @@ public class InterfaceGraphique implements Runnable {
 
         frame.getContentPane().setBackground(new Color(53, 79, 82));
         frame.setLayout(new BorderLayout());
-        InterfaceJeu  jeu = new InterfaceJeu();
-        jeu.init();
+        InterfaceJeu jeu = new InterfaceJeu();
+        jeu.init(); 
 
-        JPanel navigation = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        /*JPanel navigation = new JPanel(new FlowLayout(FlowLayout.LEFT));
         back = new Button("ressources/ButtonImage/backSR.png");
         volum = new Button("ressources/ButtonImage/volume.png");
 
         navigation.add(back);
-        navigation.add(volum);
+        navigation.add(volum);*/
+
+        // Quitter jeu
+        jeu.acceuil.quit.addMouseListener(new MouseAdapter(){
+            public void mouseClicked(MouseEvent e){
+                int quitClicked = JOptionPane.showConfirmDialog(frame, "Voulez-vous vraiment quitter le jeu ?", "Quitter", JOptionPane.YES_NO_OPTION);
+                if (quitClicked == JOptionPane.YES_OPTION){
+                    frame.dispose();
+                    System.exit(0);
+                }
+            }
+        });
 
         
         frame.add(jeu, BorderLayout.CENTER);
-        frame.add(navigation, BorderLayout.NORTH);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.add(navigation, BorderLayout.NORTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setSize(1000, 800);
+        frame.setLocationRelativeTo(null);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         frame.setUndecorated(true);
 		frame.setVisible(true);
