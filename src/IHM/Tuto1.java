6b79367b97e2a7aaf5f05c1feec03a097d729925
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-14 20:35:40 +0200

    TODO : ajout images plateau

--- src/IHM/Tuto1.java
+++ src/IHM/Tuto1.java
@@ -40,6 +40,10 @@ public class Tuto1 extends JFrame{
         JFrame frame = new JFrame();
         frame.setSize(w, h);
         frame.setLayout(new BorderLayout());
+        
+        // Background
+        Background background = new Background();
+        background.Background(frame);
 
         JPanel navig = new JPanel();
         navig.setLayout(new FlowLayout(FlowLayout.LEFT));
