a77f7fc390fa94652cf38c041c12a753b125ab48
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-14 20:33:07 +0200

    Add files via upload

---
+++ src/IHM/Background.java
@@ -0,0 +1,38 @@
+package IHM;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import java.awt.BorderLayout;
+
+import javax.imageio.ImageIO;
+
+
+public class Background {
+
+    public void Background(JFrame frame){
+
+        // Background
+        try {
+		    final Image backgroundImage = ImageIO.read(new File("ressources/ButtonImage/bg.png"));
+			
+		    frame.setContentPane(new JPanel(new BorderLayout()) {
+				private static final long serialVersionUID = 422561129403137988L;
+
+                @Override 
+                public void paintComponent(Graphics g) {
+		            g.drawImage(backgroundImage, 0, 0, frame.getWidth(), frame.getHeight(), null);
+		        }
+		    });
+		} catch (IOException e) {
+		    throw new RuntimeException(e);
+        }
+
+    }
+    
+}
