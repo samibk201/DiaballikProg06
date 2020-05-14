5f3bf0db5b250a66ebaa73a7948820bd4e2c3333
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-14 20:34:00 +0200

    Update GameSetIA.java

--- src/IHM/GameSetIA.java
+++ src/IHM/GameSetIA.java
@@ -58,6 +58,10 @@ public class GameSetIA extends JFrame{
         JFrame frame = new JFrame();
         frame.setSize(w, h);
         frame.setLayout(new BorderLayout());
+	    
+	// Background
+        Background background = new Background();
+        background.Background(frame);
 
         JPanel navig = new JPanel();
         navig.setLayout(new FlowLayout(FlowLayout.LEFT));
