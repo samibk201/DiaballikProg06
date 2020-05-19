b62fd197eb65ebf8510496a1d7601cc6e7d508d0
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-19 19:21:40 +0200

    Update LevelIA.java

--- src/IHM/LevelIA.java
+++ src/IHM/LevelIA.java
@@ -6,13 +6,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import java.awt.Font;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.Color;
 import java.awt.Image;
 import java.awt.Component;
 import java.awt.event.*;
+import java.awt.Graphics;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 
 public class LevelIA extends JFrame{
@@ -35,40 +38,28 @@ public class LevelIA extends JFrame{
         JFrame frame = new JFrame();
         frame.setSize(w, h);
         frame.setLayout(new BorderLayout());
-        
+
+
         // Background
         Background background = new Background();
-        background.Background(frame);
+        background.Background(frame, "Theme/bg.png");
 
 
         JPanel navig = new JPanel();
         navig.setLayout(new FlowLayout(FlowLayout.LEFT));
         navig.setOpaque(false);
         back = new Button("ressources/ButtonImage/backSR.png");
-        volum = new Button("ressources/ButtonImage/volume.png");
+        volum = new Button("ressources/ButtonImage/sound.png");
         navig.add(back);
         navig.add(volum);
 
 
-        // Titre
-        title = new JLabel("Choix du niveau de l'IA");
-        title.setFont(new Font("Tahoma", Font.BOLD, 56));
-        title.setAlignmentX(CENTER_ALIGNMENT);
-        title.setForeground(Color.black);
-
-        // Remplissage panel titre + navig
-        JPanel boxNT = new JPanel();
-        boxNT.setLayout(new BoxLayout(boxNT, BoxLayout.Y_AXIS));
-        boxNT.setOpaque(false);
-        boxNT.add(navig);
-        boxNT.add(title);
-
-
-        // Buttons
+        // Panel boutons
         JPanel box = new JPanel();
         box.setLayout(new BoxLayout(box, BoxLayout.Y_AXIS));
         box.setOpaque(false);
 
+        // Chargement images des boutons
         this.easy = new Button("ressources/ButtonImage/easySR.png");
         easy.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.mid = new Button("ressources/ButtonImage/moySR.png");
@@ -86,18 +77,57 @@ public class LevelIA extends JFrame{
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
         panel.setOpaque(false);
-        panel.add(boxNT, BorderLayout.NORTH);
+        panel.add(navig, BorderLayout.NORTH);
         panel.add(box, BorderLayout.CENTER);
 
 
-        // Clicks
+        // Clicks + gestion du passage de la souris sur le bouton
         back.addMouseListener(new MouseAdapter(){
+            public void mouseEntered(MouseEvent e){
+                back.setIcon(new ImageIcon("ressources/ButtonImage/backCLicked.png"));
+            }
+            public void mouseExited(MouseEvent e){
+                back.setIcon(new ImageIcon("ressources/ButtonImage/backSR.png"));
+            }
             public void mouseClicked(MouseEvent e){
                 GameSetIA gameSetIA = new GameSetIA();
                 gameSetIA.init(frame.getWidth(), frame.getHeight());
                 frame.dispose();
             }
         });
+
+        easy.addMouseListener(new MouseAdapter(){
+            public void mouseEntered(MouseEvent e){
+                easy.setIcon(new ImageIcon("ressources/ButtonImage/easyClik.png"));
+            }
+            public void mouseExited(MouseEvent e){
+                easy.setIcon(new ImageIcon("ressources/ButtonImage/easySR.png"));
+            }
+            public void mouseClicked(MouseEvent e){
+                InterfacePartie interfacePartie = new InterfacePartie();
+                interfacePartie.run();
+                frame.dispose();
+            }
+        });
+
+        mid.addMouseListener(new MouseAdapter(){
+            public void mouseEntered(MouseEvent e){
+                mid.setIcon(new ImageIcon("ressources/ButtonImage/moyClik.png"));
+            }
+            public void mouseExited(MouseEvent e){
+                mid.setIcon(new ImageIcon("ressources/ButtonImage/moySR.png"));
+            }
+        });
+
+        diff.addMouseListener(new MouseAdapter(){
+            public void mouseEntered(MouseEvent e){
+                diff.setIcon(new ImageIcon("ressources/ButtonImage/diffClik.png"));
+            }
+            public void mouseExited(MouseEvent e){
+                diff.setIcon(new ImageIcon("ressources/ButtonImage/diffSR.png"));
+            }
+        });
+        
 
 
         // Paramètrage fenêtre
