fcf1bb0a5100480827e0e7db38f8ddb0ef86edf9
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-19 19:06:06 +0200

    Update JoueurHumain.java

--- src/Moteur/JoueurHumain.java
+++ src/Moteur/JoueurHumain.java
@@ -14,7 +14,7 @@ public class JoueurHumain extends Joueur {
 	 * -La gestion des coups invalides pour redonner la commande au joueur et
 	 * Pour cela il faut changer le type de la fonction joueCoup dans JeuPlateau**/
 	
-	 @Override
+	@Override
 	public void joue (JeuPlateau jeu) {
 		int x,y,or = 0, nbCoup=0, rep;
 		boolean etat = false;
@@ -62,55 +62,51 @@ public class JoueurHumain extends Joueur {
 				
 		}
 	}
-	 @Override
-		public void joue2 (JeuPlateau jeu, int x, int y, int or) {
-			int nbCoup=0, rep;
-			boolean etat = false;
+	public void joue2 (JeuPlateau jeu, int x, int y, int or) {
+		int nbCoup=0, rep;
+		boolean etat = false;
 
-			System.out.println ("Joueur "+this.getNum () + ":"+this.getNom ());
+		System.out.println ("Joueur "+this.getNum () + ":"+this.getNom ());
 
-			while (nbCoup <3 || etat == false) {
-				jeu.afficherPlateau ();
-				System.out.println("Joueur "+this.getNom());
-				System.out.println ("Seléctionner un pion ou la balle à lancer, il vous reste "+(3-nbCoup)+" coups");
-				//x = scan.nextInt (); y = scan.nextInt ();
-				System.out.println(x + " " + y);
-				
-				System.out.println ("Entrez une direction");
-				System.out.println ("0-Pour avancer");
-				System.out.println ("1-Gauche");
-				System.out.println ("2-Droite");
-				System.out.println ("3-Arrière");
-				System.out.println ("4-Déplacement en diagonale");
+		while (nbCoup <3 || etat == false) {
+			jeu.afficherPlateau ();
+			System.out.println("Joueur "+this.getNom());
+			System.out.println ("Seléctionner un pion ou la balle à lancer, il vous reste "+(3-nbCoup)+" coups");
+			//x = scan.nextInt (); y = scan.nextInt ();
+			System.out.println(x + " " + y);
+			
+			System.out.println ("Entrez une direction");
+			System.out.println ("0-Pour avancer");
+			System.out.println ("1-Gauche");
+			System.out.println ("2-Droite");
+			System.out.println ("3-Arrière");
+			System.out.println ("4-Déplacement en diagonale");
 
-				///or = scan.nextInt ();
-				System.out.println("Action " + or);
-				if(or == 5 || nbCoup == 3)
-					etat = true;
-				etat = jeu.joueCoup (this,or,x,y);
-				if (etat == false) {
-					System.out.println (" Coup!!!!!!!Raté");
-				}
-				else
-				{
-					System.out.println ("Vous voulez déplacer le pion ("+x+","+y+") vers "+or);
-					nbCoup++;		
-					System.out.println("appuyer sur -1 pour passer le tour, 0 sinon");
-					rep = scan.nextInt();
-					if(rep == -1)
-					{
-						etat = true;
-						nbCoup = 3;
-					}
-				}
-				try {
-					Runtime.getRuntime().exec("clear");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				
-					
+			///or = scan.nextInt ();
+			System.out.println("Action " + or);
+			if(or == 5 || nbCoup == 3)
+				etat = true;
+			etat = jeu.joueCoup (this,or,x,y);
+			if (etat == false) {
+				System.out.println (" Coup!!!!!!!Raté");
 			}
+			else
+			{
+				System.out.println ("Vous voulez déplacer le pion ("+x+","+y+") vers "+or);
+				nbCoup++;		
+				System.out.println("appuyer sur -1 pour passer le tour, 0 sinon");
+				rep = scan.nextInt();
+				if(rep == -1)
+				{
+					etat = true;
+					nbCoup = 3;
+				}
+			}
+			try {
+				Runtime.getRuntime().exec("clear");
+			} catch (Exception e) {
+				e.printStackTrace();
+				}				
 		}
-
+	}
 }
