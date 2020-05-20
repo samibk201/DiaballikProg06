7493cdbb479af258057a8443ed249fd0ab53892c
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2020-05-21 00:57:01 +0200

    Add files via upload
    add Ia

---
+++ Ia/Support.java
@@ -0,0 +1,13 @@
+package Ia;
+
+public class Support {
+
+    int position;
+    int val;
+
+    public Support(int position, int val) {
+        this.position = position;
+        this.val = val;
+    }
+
+}
