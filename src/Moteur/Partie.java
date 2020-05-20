a181b65ae1eb9dd58085bc5cea63821b0ee5dd41
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-20 23:39:57 +0200

    Update Partie.java

--- src/Moteur/Partie.java
+++ src/Moteur/Partie.java
@@ -5,22 +5,16 @@ package Moteur;
 //1:Un joueur adverse dans un camp adverse
 //2:Deux joueurs adverses dans un camp adverse
 public class Partie {
-	private static Partie partie;
 	private Joueur [] joueurs = new Joueur [2];
 	private JeuPlateau jeu;
 	private int disposition;
 	
-	public Partie() {
-		
-	}
-	
-	public void init(Joueur joueur1,Joueur joueur2,int dispo) {
+	public Partie (Joueur joueur1,Joueur joueur2,int disposition) {
 		joueurs [0] = joueur1;
 		joueurs [1] = joueur2;
-		this.disposition = dispo;
-		jeu = new JeuPlateau (joueur1,joueur2,this.disposition);
+		this.disposition = disposition;
+		jeu = new JeuPlateau (joueur1,joueur2,disposition);
 	}
-	
 	/**TO DO:il va falloir gérer ici lorsqu'il y a un gagnat pour arrêter le jeu
 	 * On peut gérer cette fonction dans JeuPlateau
 	 * -idée de solution de la fonction
@@ -35,11 +29,5 @@ public class Partie {
 			joueurs [0].joue (jeu);
 			joueurs [1].joue (jeu);
 		}
-	}
-	public static Partie getInstance() {
-		if (Partie.partie == null) {
-			partie = new Partie();
-		}
-		return Partie.partie;
 	}
 }
