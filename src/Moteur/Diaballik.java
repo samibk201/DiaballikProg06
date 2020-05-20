1e98c67c8b4e5fc671dcc3d87086f9317dc24e41
BELAID Amani <58103824+LittlePinguin@users.noreply.github.com>
2020-05-20 23:38:38 +0200

    Update Diaballik.java

--- src/Moteur/Diaballik.java
+++ src/Moteur/Diaballik.java
@@ -1,15 +1,13 @@
 package Moteur;
 import java.util.Scanner;
-
-public class Diaballik {
+class Diabalik {
 	public static void main (String [] args) {
 		Scanner scan = new Scanner (System.in);
 		System.out.println ("Entrez nom1:");
 		String nom1 = scan.nextLine ();
 		System.out.println ("Entrez nom2:");
 		String nom2 = scan.nextLine ();
-		Partie partie = Partie.getInstance();
-		partie.init(new JoueurHumain (1,nom1),new JoueurHumain (2,nom2),0);
+		Partie partie = new Partie (new JoueurHumain (1,nom1),new JoueurHumain (2,nom2),1);
 		partie.joue ();
 	}
 }
